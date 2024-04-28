77aaebb9c88089aa4a4d61f2053b436b7c661fd5
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-28 23:10:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here!
-
+let infiList = document.querySelector("#infi-list");
+// console.log(infiList);
+let count = 11;
+infiList.onscroll = function (e) {
+  infiList.innerHTML += `
+  <li>Item ${count}</li>
+  <li>Item ${count + 1}</li>
+  `;
+  count += 2;
+  // console.log("element added", e);
+};
